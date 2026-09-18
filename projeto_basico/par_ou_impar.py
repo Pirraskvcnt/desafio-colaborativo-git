9635c03072514e7b6a4800fca0f83f3ac3bfdf6b
Pierre Cavalcante <pierreflores002@gmail.com>
2026-09-18 09:56:47 -0300

    Update: perguntar_nome
    Adicionei no código o input para perguntar o nome ao usuário e mostrar uma mensagem mais receptiva ao final.

--- projeto_basico/par_ou_impar.py
+++ projeto_basico/par_ou_impar.py
@@ -1,6 +1,7 @@
+nome = input("Informe o seu nome: ")
 numero = int(input("Digite um número: "))
 
 if numero % 2 == 0:
-    print("O número é par!")
+    print(f"Olá, {nome}. O número escolhido é par!")
 else:
-    print("O número é ímpar!")
+    print(f"Olá, {nome}. O número escolhido é ímpar!")
