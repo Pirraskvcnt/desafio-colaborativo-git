dd9a57b1d54058f2e384d933a30032c02c599cd4
Pierre Cavalcante <pierreflores002@gmail.com>
2026-09-18 11:04:25 -0300

    Informar nome
    Atualizei o código, para que pergunte o nome do usuário.

--- projeto_basico/par_ou_impar.py
+++ projeto_basico/par_ou_impar.py
@@ -1,7 +1,21 @@
-nome = input("Informe o seu nome: ")
-numero = int(input("Digite um número: "))
+print("== PAR OU ÍMPAR ==")
+while True:
+    nome = input("Informe o seu nome: ")
+    numero = int(input("Digite um número: "))
+    suce = numero + 1
+    ante = numero - 1
 
-if numero % 2 == 0:
-    print(f"Olá, {nome}. O número escolhido é par!")
-else:
-    print(f"Olá, {nome}. O número escolhido é ímpar!")
+    if numero % 2 == 0:
+     print(f"Olá, {nome}. O número escolhido é par!")
+     print(f"O sucessor desse número é {suce}")
+     print(f"O antecessor desse número é {ante}")
+    else:
+     print(f"Olá, {nome}. O número escolhido é ímpar!")
+     print(f"O sucessor desse número é {suce}")
+     print(f"O antecessor desse número é {ante}")
+
+    continuar = input("\nDeseja continuar? (s/n): ")
+
+    if continuar.lower() == "n":
+        print("Programa encerrado!")
+        break
