f89d5268bd0a8a45e8f5feffdc84621bfbfb79e5
Leticia Costa <leticiacostaalves27@gmail.com>
2026-09-25 10:02:30 -0300

    Update par_ou_impar.py

--- projeto_basico/par_ou_impar.py
+++ projeto_basico/par_ou_impar.py
@@ -1,18 +1,22 @@
+
 print("== PAR OU ÍMPAR ==")
+
 while True:
     nome = input("Informe o seu nome: ")
     numero = int(input("Digite um número: "))
+
     suce = numero + 1
     ante = numero - 1
+    dobro = numero * 2
 
     if numero % 2 == 0:
-     print(f"Olá, {nome}. O número escolhido é par!")
-     print(f"O sucessor desse número é {suce}")
-     print(f"O antecessor desse número é {ante}")
+        print(f"Olá, {nome}. O número escolhido é par!")
     else:
-     print(f"Olá, {nome}. O número escolhido é ímpar!")
-     print(f"O sucessor desse número é {suce}")
-     print(f"O antecessor desse número é {ante}")
+        print(f"Olá, {nome}. O número escolhido é ímpar!")
+
+    print(f"O sucessor desse número é {suce}")
+    print(f"O antecessor desse número é {ante}")
+    print(f"O dobro desse número é {dobro}")
 
     continuar = input("\nDeseja continuar? (s/n): ")
 
